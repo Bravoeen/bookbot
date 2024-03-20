383cf12c2534c6b67ff98984921b157a29a4a9b0
Bravoeen <vjlesage@gmail.com>
2024-03-20 08:51:26 +0200

    a description of your changes here

--- main.py
+++ main.py
@@ -2,33 +2,52 @@ def main():
     book_path = "/home/vincent/workspace/github.com/Bravoeen/bookbot/.gitignore/books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    char_dict = count_letters(text)
-    print(f"{num_words} words found in the document")
-    print(char_dict)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
 
-#splits words counts them
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+
 def get_num_words(text):
     words = text.split()
     return len(words)
 
-#reads the book returns a string
-def get_book_text(book_path):
-    with open(book_path) as f:
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def get_book_text(path):
+    with open(path) as f:
         return f.read()
 
 
-def count_letters(text):
-    lowered_words = text.lower()
-    num_chars = {}
-
-    for char in lowered_words:
-        if char.isalpha():
-            if char in num_chars:
-                num_chars[char] += 1
-            else:
-                num_chars[char] = 1
-
-    return num_chars
-    
-    
 main()
